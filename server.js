d957b04a28241e62c4466e29eb22f4f4faad1d43
fangye66 <fangye.yf@gmail.com>
2022-04-17 00:02:39 -0400

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express(); //create an instance of express library
 const mongoose = require('mongoose');
-const keys = require('./config/keys')
+const keys = require('./config/key')
+
+//DB config
+const db = keys.mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
 
 //create first route
 app.get('/',(req, res) => res.send('Hello World!'))
